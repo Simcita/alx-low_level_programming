f36f05a1ab4657f03e8654646946ec34c8578a67
Simcita <mlisanasimamnkele10@gmail.com>
2023-07-13 12:53:40 -0700

    Yah

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ * return is 0 if there is no error
+ * main - Main entry point
+ */
+int main(void)
+{
+	/**
+	 * where code is executed
+	 */
+
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
