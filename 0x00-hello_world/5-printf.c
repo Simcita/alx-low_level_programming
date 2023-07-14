9196126afff2bfc2d53bd03b9d13abf0f8157039
Simcita <mlisanasimamnkele10@gmail.com>
2023-07-13 17:38:31 -0700

    Yep

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program that print what is in function printf.
+ *
+ * Return: 0 on successful execution
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
