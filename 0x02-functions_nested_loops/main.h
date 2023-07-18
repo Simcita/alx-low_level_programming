7766ecee8192b95581a6fd562c6791b510df9b07
Simcita <mlisanasimamnkele10@gmail.com>
2023-07-18 14:41:04 -0700

    Awesome

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program that print what is in function puts.
+ *
+ * Return: 0 on successful execution
+ */
+
+int main(void)
+{
+	return (0);
+}
