88dcedcbc272d30e64bdef2443db753b86fbb4b1
Simcita <mlisanasimamnkele10@gmail.com>
2023-08-02 10:05:20 -0700

    Yah

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program that print what is in function puts.
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
